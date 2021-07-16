6b7ae938f3dddad3ddb67cbb172e9f98800cfb73
kintilijo <85233464+kintilijo@users.noreply.github.com>
2021-07-16 21:27:08 +0700

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -73,8 +73,7 @@ async def start_command(client: Client, message: Message):
         reply_markup = InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("😊 About Me", callback_data = "about"),
-                    InlineKeyboardButton("🔒 Close", callback_data = "close")
+                    
                 ]
             ]
         )
@@ -94,11 +93,11 @@ async def start_command(client: Client, message: Message):
 
 @Bot.on_message(filters.command('start') & filters.private)
 async def not_joined(client: Client, message: Message):
-    text = "<b>You need to join in my Channel/Group to use me\n\nKindly Please join Channel</b>"
+    text = "<b>agar video muncul anda harus join channel backup @viral_bokep1818\n\👇👇👇👇👇👇</b>"
     message_text = message.text
     try:
         command, argument = message_text.split()
-        text = text + f" <b>and <a href='https://t.me/{client.username}?start={argument}'>try again</a></b>"
+        text = text + f" <b>and <a href='https://t.me/{client.username}?start={argument}'>👉pencet ini kalau sudah join channel👈</a></b>"
     except ValueError:
         pass
     reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("Join Channel", url = client.invitelink)]])
